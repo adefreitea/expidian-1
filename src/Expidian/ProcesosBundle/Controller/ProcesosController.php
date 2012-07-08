66d3356ae5d996e6728a611b897054520a0e1c2b
Andreina <andreina@andreina.(none)>
2012-07-08 01:20:49 -0430

    ProcesosController

---
+++ src/Expidian/ProcesosBundle/Controller/ProcesosController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Expidian\ProcesosBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Expidian\GlobalBundle\Entity\Usuarios;
+use Expidian\GlobalBundle\Entity\Personas;
+
+use Expidian\UsuariosBundle\Controller\Session\SessionManager;
+
+/**
+ * Description of UsuariosController
+ *
+ * @author autana
+ */
+class ProcesosController extends Controller {
+	
+	public function indexAction()
+    {
+        
+    }
+	
+	public function indexAction()
+    {
+        
+    }
+    
+    public function newAction() 
+    {
+    
+    }
+            
+
+    public function editAction() 
+    {
+    
+    }
+    
+    public function deleteAction($id_usuario) 
+    {
+        
+    }
+    
+    
+    public function ajaxListaUsuariosAction()
+    {
+    
+    
+    }
+        
+	
+}
