1d72fdb7a8806c2a46aaf43d60ba53ca20232d8a
Andreina <andreina@andreina.(none)>
2012-07-08 23:21:21 -0430

    ProcesosController

--- src/Expidian/ProcesosBundle/Controller/ProcesosController.php
+++ src/Expidian/ProcesosBundle/Controller/ProcesosController.php
@@ -18,13 +18,39 @@ use Expidian\UsuariosBundle\Controller\Session\SessionManager;
  */
 class ProcesosController extends Controller {
 	
-	public function indexAction()
-    {
-        
-    }
 	
 	public function indexAction()
     {
+		$request = $this->getRequest();
+        $session = $request->getSession();
+                
+        $sm = new SessionManager($session);
+        $sm->readSession();
+        
+        if($sm->getSession_is_open()){
+            
+            $usuario_obj = $sm->getUsuario();
+            $breadcrumb = array(array('text'=>'Usuarios','url'=>$this->generateUrl('ExpUsuariosList')),array('text'=>'Lista','url'=>''));
+            
+            $em = $this->getDoctrine()->getEntityManager();
+            
+            if($usuario_obj->getPerfil()->getPerfil()=='Administrador' || $usuario_obj->getPerfil()->getPerfil()=='Abogado Coordinador'){
+                $isSearch = false; 
+                $searchField = ""; 
+                $searchOper = ""; 
+                $searchString = ""; 
+                $direction = "ASC"; 
+                $sort = "u.idUsuario"; 
+                $rows = "10"; 
+                $page = "1";
+                return $this->render('ExpidianUsuariosBundle:Usuarios:index.html.twig', array('breadcrumb'=>$breadcrumb,'usuario'=>$usuario_obj, 'is_search'=>$isSearch, 'searchField'=>$searchField, 'searchOper'=>$searchOper, 'searchString'=>$searchString, 'direction'=>$direction, 'sort'=>$sort, 'rows'=>$rows, 'page'=>$page));
+            }else{
+                return $this->render('ExpidianGlobalBundle::dialog_msj.html.twig', array('msj'=>'Su perfil no posee permisos para efectuar esta acci&oacute;n en el sistema.','class'=>'errorDialogBox', 'breadcrumb'=>$breadcrumb,'usuario'=>$usuario_obj));
+            }
+        }else{
+            $estatus = "inicio";
+            return $this->render('ExpidianUsuariosBundle:Login:login.html.twig', array('accion'=>'login','estatus'=>$estatus));
+        } 
         
     }
     
@@ -34,18 +60,18 @@ class ProcesosController extends Controller {
     }
             
 
-    public function editAction() 
+    public function editAction($id_proceso) 
     {
     
     }
     
-    public function deleteAction() 
+    public function deleteAction($id_proceso) 
     {
         
     }
     
     
-    public function ajaxListaUsuariosAction()
+    public function ajaxListaUsuariosAction($page)
     {
     
     
---
+++ src/Expidian/ProcesosBundle/Form/ProcesosType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Expidian\GlobalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class PersonasType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('idProceso','hidden')
+            ->add('proceso')
+            ->add('procesoAbrev')
+            ->add('nroConsecutivoProceso','number')        
+        ;
+    }
+           
+    public function getName()
+    {
+        return 'procesostype';
+    }
+}
